5e1422b4b8dea9c87c52f474b9732d9b8414eb7d
Alpaca <paulocastrogalvao@hotmail.com>
2024-10-20 18:30:56 +0900

    Add screen_ocr file

---
+++ screen_ocr.py
@@ -0,0 +1,53 @@
+import keyboard
+import tempfile
+import pyperclip
+import subprocess
+import clipboard_monitor
+import pytesseract as pyte
+
+from PIL import ImageGrab
+
+# Todo: handle tesseract not found
+tesseract_path = r'D:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+tesseract_config = r"-l eng+jpn+jpn_vert --psm 6"
+ocr_hotkey = 'ctrl+F1'
+is_screenshot_valid = False
+
+
+def on_clipboard_image_update():
+    global is_screenshot_valid
+
+    if not is_screenshot_valid:
+        return
+
+    image = ImageGrab.grabclipboard()
+
+    result_string = pyte.image_to_string(image, config=tesseract_config)
+    pyperclip.copy(result_string)
+
+    is_screenshot_valid = False
+
+
+def execute_screen_ocr(*_):
+    global is_screenshot_valid
+
+    _ = subprocess.call(['explorer.exe', 'ms-screenclip:'])
+    is_screenshot_valid = True
+
+
+def on_update_dummy():
+    pass
+
+
+def main():
+    pyte.pytesseract.tesseract_cmd = tesseract_path
+
+    clipboard_monitor.on_image(on_clipboard_image_update)
+    clipboard_monitor.on_update(lambda: None)
+
+    keyboard.add_hotkey(ocr_hotkey, execute_screen_ocr, args =('hotkey', 'detected'))
+    keyboard.wait()
+
+
+if __name__ == '__main__':
+    main()
